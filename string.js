fb67566b3cca28f5299b09a825dae450026fd69a
vital <66871109+vital345@users.noreply.github.com>
2021-09-30 21:00:56 +0530

    Update string.js
    onmoseover event handler added

--- string.js
+++ string.js
@@ -5,12 +5,19 @@
 <h2>JavaScript Expressions</h2>
 
 <p>Expressions compute to values.</p>
-
-<p id="demo"></p>
+<p>The function bigImg() is triggered when the user moves the mouse pointer over the image.</p>
+<p>The function normalImg() is triggered when the mouse pointer is moved out of the image.</p>
 
 <script>
-document.getElementById("demo").innerHTML = "John" + " "  + "Doe";
-</script>
+function bigImg(x) {
+  x.style.height = "64px";
+  x.style.width = "64px";
+}
 
+function normalImg(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
+}
+</script>
 </body>
 </html>
